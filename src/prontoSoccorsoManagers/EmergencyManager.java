a2abbd89eb09d28444e58e36ce3e3e9fa21449c3
MauroGariazzo <mauromggariazzo@gmail.com>
2024-08-05 21:26:13 +0200

    add EmergencyManager
    aggiunto classe EmergencyManager

--- src/prontoSoccorsoManagers/EmergencyManager.java
+++ src/prontoSoccorsoManagers/EmergencyManager.java
@@ -1,20 +1,20 @@
-package prontoSoccorsoManagers;
-
+package prontoSoccorso;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-import prontoSoccorso.Emergency;
+/*import prontoSoccorso.Emergency;
 import prontoSoccorso.Patient;
 import prontoSoccorso.StaffMember;
-import prontoSoccorso.Turn;
+import prontoSoccorso.Turn;*/
 import prontoSoccorso.Turn.TimeSlot;
 
 public class EmergencyManager implements CRUD<Emergency>{
 	
 	List<Emergency> emergencies = new ArrayList<>();
+	
 	StaffManager staffManager;
 	TurnManager turnManager;
 
@@ -25,38 +25,44 @@ public class EmergencyManager implements CRUD<Emergency>{
 
 	@Override
 	public void add(Emergency o) {
-		// TODO Auto-generated method stub
-		
-	}
-
+		emergencies.add(o);
+	}		
+	
+	
 	@Override
 	public Emergency get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return emergencies.get(index);
 	}
 
 	@Override
 	public boolean remove(int index) {
-		// TODO Auto-generated method stub
+		emergencies.remove(index);
 		return false;
 	}
 
 	@Override
 	public boolean update(int index, Emergency o) {
-		// TODO Auto-generated method stub
+		if(index >= 0 && index <= emergencies.size()-1)
+		{
+			emergencies.set(index, o);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return emergencies.size() == 0;
 	}
 
 	@Override
-	public List<Emergency> filter(Predicate<Emergency> p) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Emergency> filter(Predicate<Emergency> p) {		
+		List<Emergency> temp = new ArrayList<>();
+    	for(Emergency emergency : emergencies) {
+    		if(p.test(emergency))
+    			temp.add(emergency);	
+    	}
+		return temp;
 	}
 
 	public List<StaffMember> filterByTimeSlot(LocalDateTime date) {
@@ -64,17 +70,14 @@ public class EmergencyManager implements CRUD<Emergency>{
 		List<Turn> turns = turnManager.filter(p -> p.getSlot().equals(slot));
 		for(Turn turn : turns) {
 			if(date.isAfter(turn.getStart()) && date.isBefore(turn.getEnd())) { //if date is between two dates
-				List<StaffMember> staffMembers = turn.getAssignedStaff();
-				if(staffMembers.isEmpty()) 
-					return null;
-				return staffMembers;
+				return turn.getAssignedStaff();
 			}
 		}
 		return null;
 	}
+	
 	@Override
-	public void forEach(Consumer<Emergency> c) {
-		// TODO Auto-generated method stub
-		
+	public void forEach(Consumer<Emergency> e) {
+		emergencies.forEach(e);
 	}
 }
